3083803e03af79867eea82f3f3912430a6c37bb9
murandot <103624383+murandot@users.noreply.github.com>
2022-05-08 23:15:32 +0900

    ActiveHashのモデルクラスの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,25 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :schedule
   
+  validates :image, presence:true
   validates :name, presence: true
   validates :content, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :schedule_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :schedule_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, 
+                    format: { with: /\A[0-9]+\z/ },
+                    allow_blank: true
   validates :price, presence: true
-  
 end
