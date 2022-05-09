6f2565a6f3ff900a8ceb0c8ed7d7599bb19854b2
murandot <103624383+murandot@users.noreply.github.com>
2022-05-09 22:27:13 +0900

    renameファイルの削除

--- db/migrate/20220508105543_rename_category_column_to_items.rb
+++
@@ -1,6 +0,0 @@
-class RenameCategoryColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :category, :category_id
-    rename_column :items, :status, :status_id
-  end
-end
