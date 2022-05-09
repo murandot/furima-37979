732113f557e1d8d6485b34795750ed2475a2bfc6
murandot <103624383+murandot@users.noreply.github.com>
2022-05-09 21:29:29 +0900

    Itemテーブルのカラムの再修正

--- db/migrate/20220506090758_create_items.rb
+++ db/migrate/20220506090758_create_items.rb
@@ -3,12 +3,12 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string     :name, null: false
       t.text       :content, null: false
-      t.integer    :category_id, null: false
-      t.integer    :status_id, null: false
+      t.integer    :category, null: false
+      t.integer    :status, null: false
       t.integer    :fee_id, null: false
       t.integer    :prefecture_id, null: false
       t.integer    :schedule_id, null: false
-      t.integer    :price, null: false
+      t.integer    :price_id, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
