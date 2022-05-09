2ba240654f94f2af9d81dc5e18699a156520e147
murandot <103624383+murandot@users.noreply.github.com>
2022-05-09 17:03:57 +0900

    アソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname, presence: true
 
   with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } do
