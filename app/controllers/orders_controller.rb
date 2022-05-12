57acb494f1353f9d3f73d39edc21df431906a202
murandot <103624383+murandot@users.noreply.github.com>
2022-05-12 12:05:56 +0900

    Formオブジェクトパターンの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,35 @@
 class OrdersController < ApplicationController
-  def index
-    @item = Item.find(params[:item_id])
-  end
 
+  def index
+    set_item
+    if current_user == @item.user || Order.exists?(item_id: @item.id)
+      redirect_to root_path
+    elsif current_user !=  @item.user && !Order.exists?(item_id: @item.id)
+      @order_address = OrderAddress.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+  
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      set_item
+      render :index
+    end
+  end
+  
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+  
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  validates :postal_code,   presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-  validates :prefecture_id, presence: true, numericality: {other_than: 1}
-  validates :city,   presence: true
-  validates :house_number,   presence: true
-  validates :building_name,   presence: true
-  validates :phone_number,   presence: true, format: { with: /\A\d{10,11}\z/ }
-
 end
